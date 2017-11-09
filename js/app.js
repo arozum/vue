3d463fb54aa31e56d2fbd1679b14369d5c2c88cb
arozum <rozumek@hotmail.com>
2017-11-09 22:05:57 +0100

    ..components

--- js/app.js
+++ js/app.js
@@ -10,7 +10,15 @@ var app = new Vue({
         editing: false,
         complete: false
       }],
-      currTodo: ""
+      currTodo: "",
+      website: "http://www.winzo.pl",
+      htmlTag: "<a href='/test'>link html from model</a>",
+      x: 0,
+      y: 0,
+      isDynamicRed: false,
+      isShow: false,
+      isError: false,
+      output: 'Nothing'
     },
     methods: {
       addTodo: function() {
@@ -31,6 +39,23 @@ var app = new Vue({
       },
       changeCheck: function(todo) {
         todo.complete = !todo.complete;
+      },
+      updateXY: function(event){
+        this.x = event.offsetX;
+        this.y = event.offsetY;
+      },
+      beforeLink: function(){
+        alert("Are you ready?");
+      },
+      logName: function(){
+        console.log("log name by enter");
+      },
+      logAge: function(){
+        console.log("log age");
+      },
+      readRefs: function() {
+        console.log(this.$refs);
+        this.output =  this.$refs.input.value;
       }
     },
     computed: {
@@ -41,4 +66,27 @@ var app = new Vue({
         return this.todos.filter(function(item){ return !item.complete })
       }
     }
+  })
+
+  //////////////////////////
+  Vue.component('samplecomp', {
+    template: '<p>Hi, I am {{name}}. <button v-on:click="changeName">Change name</button></p>',
+    data: function(){
+      return {
+        name: 'Tom'
+      }
+    },
+    methods: {
+      changeName: function(){
+        this.name = 'Mario';
+      }
+    }
+  })
+
+  new Vue({
+    el: '#vue-app-one'
+  })
+
+  new Vue({
+    el: '#vue-app-two'
   })
